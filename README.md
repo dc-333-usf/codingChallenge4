d4b7bd9b1e3ad1824eef23ce4cfd129b12225777
Drew Cook <dc333@usf.edu>
2025-02-02 22:58:38 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Hello, this is my fourth coding challenge in my business application development class.
---
+++ cc_4.js
@@ -0,0 +1,9 @@
+//Task 1 If statements
+let purchaseAmount = 70; //declare purchase amount
+if (purchaseAmount > 100) {
+    finalAmount = purchaseAmount * 0.9;
+} else {
+    finalAmount = purchaseAmount;
+}; //write if statement for a 10 percent discount if order amount is over 100
+console.log(`Final amount: ${finalAmount}`); //log final purchase amount
+
